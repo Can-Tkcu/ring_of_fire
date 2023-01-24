33973878531890e5cb60651fc9f0cecf097d07ab
cantkcu <can.tkcu@hotmail.com>
2023-01-24 11:49:48 +0100

    first playable version

---
+++ src/app/dialog-add-player/dialog-add-player.component.ts
@@ -0,0 +1,18 @@
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-dialog-add-player',
+  templateUrl: './dialog-add-player.component.html',
+  styleUrls: ['./dialog-add-player.component.scss'],
+})
+
+export class DialogAddPlayerComponent {
+   public name: string = '';
+
+  constructor( public dialogRef: MatDialogRef<DialogAddPlayerComponent> ) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+}
---
+++ src/app/game/game.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Game } from 'src/models/game';
+import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.scss'],
+})
+export class GameComponent implements OnInit {
+  pickCardAnimation = false;
+  currentCard: string = '';
+  game: Game = new Game();
+
+  constructor(public dialog: MatDialog) {}
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogAddPlayerComponent, {});
+
+    dialogRef.afterClosed().subscribe((name: string) => {
+      if(name)
+      this.game.players.push(name.replace('<', '&gt').trim().replace('>', '&lt'));
+    });
+  }
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+  }
+
+  takeCard() {
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop()!;
+      this.pickCardAnimation = true;
+      this.game.currentPlayer++;
+      this.game.currentPlayer = this.game.currentPlayer % this.game.players.length;
+
+      setTimeout(() => {
+        this.game.playedCards.push(this.currentCard);
+        this.pickCardAnimation = false;
+      }, 1500);
+    }
+  }
+}
